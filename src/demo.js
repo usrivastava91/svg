fbee1feedf3ec1042107e9fe3b7f80a14d7f84da
Utkarsh Srivastava <utkarsh.srivastava.consultant@nielsen.com>
2017-02-22 20:20:40 +0530

    first commit

---
+++ src/demo.js
@@ -0,0 +1,23 @@
+// (function(){
+//     console.log("Demoplate Works!");
+// }())
+
+var btns = document.querySelectorAll(".buttons button");
+var cat = document.getElementsByClassName("cat")[0];
+
+for (i = 0; i< btns.length; i++){
+btns[i].addEventListener("click", manageCatClasses);
+}
+
+function manageCatClasses() {
+    if (this.getAttribute("data-add")) {
+    cat.classList.add(this.getAttribute("data-add"));
+    }
+
+   if (this.getAttribute("data-remove")) {
+    cat.classList.remove(this.getAttribute("data-remove"));
+    }
+
+
+    
+}
